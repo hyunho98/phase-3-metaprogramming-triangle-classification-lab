4565fc2196863af831995188261e080bf2d5e66a
Alex Lee <hyunho98@gmail.com>
2023-06-28 18:04:06 -0400

    commit tree e182d58573641abf5d848ce24f0d3db788cc751c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+    @sum_one = first + second
+    @sum_two = first + third
+    @sum_three = second + third
+  end
+
+  def kind
+    if @sum_one > @third && @sum_two > @second && @sum_three > @first
+      if @first == @second || @first == @third || @second == @third
+        if @sum_three / 2 == @first
+          return :equilateral
+        end
+        return :isosceles
+      end
+      return :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end    
 end
